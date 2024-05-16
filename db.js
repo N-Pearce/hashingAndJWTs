705e8de8be04794fc3b91e8bc2eebe18d4c6adb2
skyla <nathanchrispearce@gmail.com>
2024-05-15 20:37:17 -0600

    first commit, project doesn't work

---
+++ db.js
@@ -0,0 +1,15 @@
+/** Database connection for messagely. */
+
+
+const { Pool } = require("pg");
+const { DB_URI } = require("./config");
+
+const pool = new Pool({
+    DB_URI,
+    password: process.env.PASSWORD,
+    host: 'localhost',
+    user: 'postgres',
+    port: 5432
+});
+
+module.exports = pool;
---
+++ routes/users.js
@@ -0,0 +1,44 @@
+const { Router } = require('express')
+const router = new Router();
+
+/** GET / - get list of users.
+ *
+ * => {users: [{username, first_name, last_name, phone}, ...]}
+ *
+ **/
+
+router.get('/', async (req, res, next) => {
+    const m = {message: "hi!"}
+    return res.json(m)
+})
+
+
+/** GET /:username - get detail of users.
+ *
+ * => {user: {username, first_name, last_name, phone, join_at, last_login_at}}
+ *
+ **/
+
+
+/** GET /:username/to - get messages to user
+ *
+ * => {messages: [{id,
+ *                 body,
+ *                 sent_at,
+ *                 read_at,
+ *                 from_user: {username, first_name, last_name, phone}}, ...]}
+ *
+ **/
+
+
+/** GET /:username/from - get messages from user
+ *
+ * => {messages: [{id,
+ *                 body,
+ *                 sent_at,
+ *                 read_at,
+ *                 to_user: {username, first_name, last_name, phone}}, ...]}
+ *
+ **/
+
+module.exports = router
